6391922a488f610506e56d3e4c96b0eac451f24c
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-12-05 23:13:55 +0200

    upgrade rx-react-component

--- src/components/test.js
+++ src/components/test.js
@@ -1,12 +1,12 @@
 import di from 'di1';
-import RxContainer from 'rx-react-container';
+import createContainer from 'rx-react-container';
 
 import Test from '../views/Test';
 
 export default di.annotate(
   (data)=>
     (params)=> {
-      return new RxContainer(Test, {list: data}, {}, {name: params.name});
+      return createContainer(Test, {list: data}, {}, {name: params.name});
     },
   require('../stores/data')
 );
